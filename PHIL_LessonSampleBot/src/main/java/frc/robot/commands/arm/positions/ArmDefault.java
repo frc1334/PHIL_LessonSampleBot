0a28b19a782e6baf75726fa46664f9ab2e6e71c6
Rico Zhu <33135503+ricozhuthegreat@users.noreply.github.com>
2019-09-29 11:46:05 -0400

    Create ArmDefault

---
+++ PHIL_LessonSampleBot/src/main/java/frc/robot/commands/arm/positions/ArmDefault.java
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.arm.positions;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+import frc.robot.commands.arm.*;
+
+public class ArmDefault extends CommandGroup {
+
+  public ArmDefault() {
+    // Add a new sequential command called MoveArm to the potentiometer tick position 470
+    addSequential(new MoveArm(470));
+  }
+
+}
